9fddee137c68a3d18a6f67646f441581dc31a2e2
Moyesh Khanal <Moyeshkhanal@gmail.com>
2021-04-13 22:28:41 -0400

    Updated command permission so only Dev can send the option alerts

--- cmds/Dev.py
+++ cmds/Dev.py
@@ -6,35 +6,43 @@ class Dev(commands.Cog):
         self.bot = bot
 
     @commands.has_role("Admin")
-    @commands.command(aliases=[ "o", "op", "entry"])
+    @commands.command(aliases=["o", "op", "entry"])
     async def option(self, ctx, *alert):
-        embed = discord.Embed(title=alert[0], color=discord.Color.green())
-        embed.add_field(name="Strike Price", value=alert[1])
-        embed.add_field(name="Expiration", value=alert[2])
-        embed.set_footer(text="Make sure to follow 20% stop loss, take profits when happy with profits!")
-        channel = self.bot.get_channel(816498810394116106)
-        await channel.send("<@&815793376862404659>", embed=embed)
-
+        if ctx.author == "UNKNOWN#5203" or ctx.author == "nayan#4761":
+            embed = discord.Embed(title=alert[0], color=discord.Color.green())
+            embed.add_field(name="Strike Price", value=alert[1])
+            embed.add_field(name="Expiration", value=alert[2])
+            embed.set_footer(text="Make sure to follow 20% stop loss, take profits when happy with profits!")
+            channel = self.bot.get_channel(816498810394116106)
+            await channel.send("<@&815793376862404659>", embed=embed)
+        else:
+            await ctx.send("You do not have permission to use this command")
 
     @commands.has_role("Admin")
     @commands.command(aliases=["s", "sw"])
     async def swing(self, ctx, *alert):
-        embed = discord.Embed(title=alert[0], color=discord.Color.green())
-        embed.add_field(name="Strike Price", value=alert[1])
-        embed.add_field(name="Expiration", value=alert[2])
-        embed.set_footer(text="Make sure to follow 20% stop loss, take profits when happy with profits!")
-        channel = self.bot.get_channel(826996954902691850)
-        await channel.send("<@&815793376862404659>", embed=embed)
+        if ctx.author == "UNKNOWN#5203" or ctx.author == "nayan#4761":
+            embed = discord.Embed(title=alert[0], color=discord.Color.green())
+            embed.add_field(name="Strike Price", value=alert[1])
+            embed.add_field(name="Expiration", value=alert[2])
+            embed.set_footer(text="Make sure to follow 20% stop loss, take profits when happy with profits!")
+            channel = self.bot.get_channel(826996954902691850)
+            await channel.send("<@&815793376862404659>", embed=embed)
+        else:
+            await ctx.send("You do not have permission to use this command")
 
     @commands.has_role("Admin")
     @commands.command(aliases=["d", "dt"])
     async def day(self, ctx, *alert):
-        embed = discord.Embed(title=alert[0], color=discord.Color.green())
-        embed.add_field(name="Strike Price", value=alert[1])
-        embed.add_field(name="Expiration", value=alert[2])
-        embed.set_footer(text="Make sure to follow 20% stop loss, take profits when happy with profits!")
-        channel = self.bot.get_channel(826996797376692234)
-        await channel.send("<@&815793376862404659>", embed=embed)
+        if ctx.author == "UNKNOWN#5203" or ctx.author == "nayan#4761":
+            embed = discord.Embed(title=alert[0], color=discord.Color.green())
+            embed.add_field(name="Strike Price", value=alert[1])
+            embed.add_field(name="Expiration", value=alert[2])
+            embed.set_footer(text="Make sure to follow 20% stop loss, take profits when happy with profits!")
+            channel = self.bot.get_channel(826996797376692234)
+            await channel.send("<@&815793376862404659>", embed=embed)
+        else:
+            await ctx.send("You do not have permission to use this command")
 
     # Send the user error if no ticker is found
     @commands.Cog.listener()
